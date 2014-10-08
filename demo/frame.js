c699a5fe5d183d17fc65dd86eb2d876effefea0b
Adrian May <adrian.alexander.may@gmail.com>
2014-10-08 17:27:09 +0800

    Foo

--- demo/frame.js
+++ demo/frame.js
@@ -3,16 +3,17 @@ function go() {
   function tabulate(l,r) {
     return "<table border='1' width='100%'><tr><th>Source<th>Result<tr style='vertical-align:top;'><td><pre>"+l+"</pre><td><pre>"+r+"</pre></table>"
   }
-  var log = [];
-  meat(log);
-  var right = log.join("\n") + "\n\n\n";
-  var left = "";
-  
   function makeLink(title, url) { return "<a href='"+url+"'>"+title+"</a>"; }
   function maybeAddLinkToId(title, id) {
     var el = document.getElementById(id);
     return  el ? "<p>"+makeLink(title, el.getAttribute('src')) : "";
   }
+  var log = [];
+  meat(log);
+  var right = log.join("\n") + "\n\n\n";
+  right += maybeAddLinkToId("Next Demo", 'next'); 
+  var left = "";
+  
   left += maybeAddLinkToId("Underlying library", 'core'); 
   left += maybeAddLinkToId("Overlying library", 'extra'); 
   left += document.getElementById('meat').innerHTML;
