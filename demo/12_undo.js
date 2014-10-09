a3f59492cea79fa8db8f7ca969d932fb61855dcd
Adrian May <adrian.alexander.may@gmail.com>
2014-10-09 12:15:41 +0800

    Tidy

--- demo/12_undo.js
+++ demo/12_undo.js
@@ -8,13 +8,18 @@ Object.prototype.edit = function (p, v, log) {
     else
       target[s] = (typeof v === 'object') ? v.project() : v;
   }
-  if (log!==undefined) log.push(JSON.stringify(flatten(ob)));
+  if (log!==undefined) 
+    log.push(JSON.stringify(flatten(ob))); 
+    //flatten brings all inherited properties into ultimate object
+    // recursing for object-valued properties. It doesn't touch
+    // the parameter
   return ob;
 }
 
 Object.prototype.undo = function (log) {
-  var ob = Object.getPrototypeOf(this);
-  if (log!==undefined) log.push(JSON.stringify(flatten(ob)));
-  return ob
+  var ob = this.proto();
+  if (log!==undefined) 
+    log.push(JSON.stringify(flatten(ob)));
+  return ob;
 }
 
