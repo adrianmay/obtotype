c2ebda952a649ec9ac498ca3480023aa9c18623e
Adrian May <adrian.alexander.may@gmail.com>
2014-10-10 10:03:11 +0800

    Messaging

--- demo/navs.js
+++ demo/navs.js
@@ -3,6 +3,7 @@ var navs = {
   '12_undo.html': { prev: '11_basic.html', next: '13_dir.html' }, 
   '13_dir.html': { prev: '12_undo.html', next: '14_box.html' }, 
   '14_box.html': { prev: '13_dir.html', next: '15_rect.html' }, 
-  '15_rect.html': { prev: '14_box.html', next: '21_basic.html' }, 
-  '21_basic.html': { prev: '15_rect.html', next: '-' }, 
+  '15_rect.html': { prev: '14_box.html', next: '16_message.html' }, 
+  '16_message.html': { prev: '15_rect.html', next: '21_basic.html' }, 
+  '21_basic.html': { prev: '16_message.html', next: '-' }, 
 }
